067209e90fd0ed42ad74899c167e8a3d1c161cbd
Narcis Neacsu <narcis.neacsu@doc-process.com>
2018-07-09 16:52:11 +0300

    Added logging functionality to camel route.

--- src/main/java/com/company/file/CopyFilesCamelLogging.java
+++ src/main/java/com/company/file/CopyFilesCamelLogging.java
@@ -4,7 +4,7 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class CopyFilesCamel {
+public class CopyFilesCamelLogging {
     public static void main(String[] args) {
         CamelContext context = new DefaultCamelContext();
 
@@ -13,6 +13,7 @@ public class CopyFilesCamel {
                 @Override
                 public void configure() throws Exception {
                     from("file:data/input?noop=true")
+                            .to("log:?level=INFO&showBody=true&showHeaders=true")
                             .to("file:data/output");
                 }
             });
